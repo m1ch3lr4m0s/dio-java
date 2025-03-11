1cd5b43f0b5a1f15cd39c30b964fbda44275d6dc
michel <programmer.michel@gmail.com>
2025-03-11 14:14:12 -0300

    estrutura do while

--- projeto-inicial/src/App.java
+++ projeto-inicial/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean end = false;
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println("The number is: " + number);
+            System.out.println("Do you want to continue? (Y/N)");
+            String response = scanner.next();
+            if(response.equalsIgnoreCase("N")){
+                end = true;
+            }
+            else if(response.equalsIgnoreCase("Y")){
+                end = false;
+            }
+            else{
+                System.out.println("Invalid response. Please enter Y or N.");
+            }
+        }while(!end);
+        scanner.close();
     }
 }
