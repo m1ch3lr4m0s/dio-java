763328c5ba5a6619be608c9ddbf3ad3364482f7c
michel <programmer.michel@gmail.com>
2025-03-13 12:04:00 -0300

    Teste com interface aplicado, usando exemplo de uma calculadora de linha de comando.

--- projeto-inicial/src/App.java
+++ projeto-inicial/src/App.java
@@ -1,14 +1,66 @@
 import java.util.Scanner;
+
+interface calculadora{
+    public int somar(int a, int b);
+    public int subtrair(int a, int b);
+    public int multiplicar(int a, int b);
+    public int dividir(int a, int b);
+}
 public class App {
     public static void main(String[] args) throws Exception {
         boolean end = false;
         Scanner scanner = new Scanner(System.in);
 
+        // Calculadora
+        calculadora calc = new calculadora(){
+            @Override
+            public int somar(int a, int b){
+                return a + b;
+            }
+            @Override
+            public int subtrair(int a, int b){
+                return a - b;
+            }
+            @Override
+            public int multiplicar(int a, int b){
+                return a * b;
+            }
+            @Override
+            public int dividir(int a, int b){
+                return a / b;
+            }
+        };
+
         do{
-            System.out.println("Enter a number: ");
-            int number = scanner.nextInt();
-            System.out.println("The number is: " + number);
-            System.out.println("Do you want to continue? (Y/N)");
+            int operacao = 0;
+            System.out.println("Informe a operação desejada: ");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão");
+            operacao = scanner.nextInt();
+            System.out.println("Informe o primeiro número: ");
+            int num1 = scanner.nextInt();
+            System.out.println("Informe o segundo número: ");
+            int num2 = scanner.nextInt();
+            switch(operacao){
+                case 1:
+                    System.out.println("O resultado da soma é: " + calc.somar(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("O resultado da subtração é: " + calc.subtrair(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("O resultado da multiplicação é: " + calc.multiplicar(num1, num2));
+                    break;
+                case 4:
+                    System.out.println("O resultado da divisão é: " + calc.dividir(num1, num2));
+                    break;
+                default:
+                    System.out.println("Operação inválida");
+                    break;
+            }
+            System.out.println("Deseja continuar? (Y/N)");
             String response = scanner.next();
             if(response.equalsIgnoreCase("N")){
                 end = true;
